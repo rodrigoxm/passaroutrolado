2e17f1216e59e057c09b162126fcb3a7b9d2fa7a
rodrigo <102531891+rodrigoxm@users.noreply.github.com>
2023-06-28 09:03:29 -0300

    Add files via upload

---
+++ calculo.js
@@ -0,0 +1,9 @@
+var primeiro;
+var segundo;
+var sinal;
+
+function getSete(){
+    document.getElementById('visor').value = "7"
+}
+function getSete(){
+    document.getElementById('visor').value = "8"
